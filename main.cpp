b3c85b4b637cc3fcd634adff4e065c4d25cbc125
Тимофеев Антон Павлович <a.timofeev.2017@stud.nstu.ru>
2020-09-22 13:22:52 +0700

    Добавить суперкласс оружия Добавить класс меча

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include "weaponbehavior.h"
+#include "swordbehavior.h"
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    WeaponBehavior *wb = new SwordBehavior();
+    wb->useWeapon();
+    return a.exec();
+}
---
+++ swordbehavior.cpp
@@ -0,0 +1,9 @@
+#include "swordbehavior.h"
+
+SwordBehavior::SwordBehavior()
+{
+
+}
+void SwordBehavior::useWeapon(){
+    cout<< "Udar mechom"<< endl;
+}
